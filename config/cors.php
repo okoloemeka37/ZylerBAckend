ddfffccca89aa7b92e6b0e00b60d891a91b8c6e6
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2025-03-31 22:33:36 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://zylerfrontend.onrender.com/', 'http://localhost:3000'],
+    'allowed_origins' => ['https://zylerfrontend.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
